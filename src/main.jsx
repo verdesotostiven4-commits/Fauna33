53b8eb8da13518f2bf075599174be52203fd655a
verdesotostiven4-commits <verdesotostiven4@gmail.com>
2026-06-16 14:31:42 -0500

    Importar estilos de imágenes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import './scheme.css';
+import './familyImages.css';
 import './familyImages.js';
 
 createRoot(document.getElementById('root')).render(<App />);
